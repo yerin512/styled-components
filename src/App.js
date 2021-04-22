1acbff79f6c53e403950443c0701cf491505f5b0
yerin <73337811+yerin512@users.noreply.github.com>
2021-04-22 23:05:14 +0900

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button>Hello</Button>
+      <Button danger>Hello</Button>
+    </Container>
+
   );
+ }
 }
+
+const Container = styled.div`
+height: 100vh;
+width:100%;
+background-color:pink;
+`;
+
+const Button = styled.button`
+border-radius:50px;
+padding: 5px;
+min-width:120px;
+color:#fff;
+font-weight:600;
+-webkit-appearance:none;
+cursor:pointer;
+&:active,
+&:focus {
+  outline:none;
+}
+background-color ${props => (props.danger ? "#e74c3c": "#2ecc71")};
+`
 
 export default App;
